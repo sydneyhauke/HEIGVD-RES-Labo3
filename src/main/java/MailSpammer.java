7b11a451527f43134276f55ea91bfae5c0b81274
Thuy-My <thuy-my.tran@epfl.ch>
2017-04-06 18:14:27 +0200

    Update

--- src/main/java/MailSpammer.java
+++ src/main/java/MailSpammer.java
@@ -1,7 +1,49 @@
-package PACKAGE_NAME;
+import data.EmptyStoreException;
+import data.MailGroup;
+import data.MessageStore;
+import protocol.Smtp;
+import sun.plugin2.message.Message;
 
-/**
- * Created by Thuy-My on 06.04.2017.
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MailSpammer {
+
+    public static void main(String[] args) throws IOException, EmptyStoreException {
+        String server = "mailcl0.heig-vd.ch";
+        String domain = "heig-vd.ch";
+        int port = 25;
+
+        Socket clientSocket = new Socket(server, port);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
+
+        MailGroup mailgroup = new MailGroup(file_emails);
+        List<String> mailList = new ArrayList<String>();
+
+        MessageStore messageStore = new MessageStore(file_messages);
+
+        /* Random emails sending */
+
+        output.println(Smtp.CMD_EHLO + domain);
+        output.println(Smtp.CMD_MAIL_FROM + mailgroup.getSender());
+
+        for(String mail : mailList) {
+            output.println(Smtp.CMD_RCPT_TO + mail);
+        }
+
+        output.println(Smtp.CMD_DATA);
+
+        output.println(messageStore.getRandomMessage());
+
+        output.println(Smtp.CMD_END_OF_DATA);
+        output.flush();
+    }
+
 }
--- src/main/java/protocol/Smtp.java
+++ src/main/java/protocol/Smtp.java
@@ -4,7 +4,7 @@ package protocol;
 public final class Smtp {
     public static final String CMD_EHLO = "EHLO";
     public static final String CMD_MAIL_FROM = "MAIL FROM:";
-    public static final String CMD_RCPT_TO = "RCPT_TO:";
+    public static final String CMD_RCPT_TO = "RCPT TO:";
     public static final String CMD_DATA = "DATA";
     public static final String CMD_END_OF_DATA = ".";
 }
