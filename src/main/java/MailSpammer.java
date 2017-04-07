d5f04ec69f7fc5ecf057498c7fa45707c4192345
Sydney Hauke <sydney.hauke@gmail.com>
2017-04-07 15:52:20 +0200

    Corrected and tested the spammer system

--- src/main/java/MailSpammer.java
+++ src/main/java/MailSpammer.java
@@ -2,48 +2,90 @@ import data.EmptyStoreException;
 import data.MailGroup;
 import data.MessageStore;
 import protocol.Smtp;
-import sun.plugin2.message.Message;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MailSpammer {
+    private static Logger LOG;
 
     public static void main(String[] args) throws IOException, EmptyStoreException {
-        String server = "mailcl0.heig-vd.ch";
-        String domain = "heig-vd.ch";
-        int port = 25;
+        LOG = Logger.getLogger(MailSpammer.class.getName());
+
+        String server = "localhost";
+        String domain = "sydney";
+        int port = 2525;
 
         Socket clientSocket = new Socket(server, port);
 
         BufferedReader input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter output = new PrintWriter(clientSocket.getOutputStream());
 
-        MailGroup mailgroup = new MailGroup(file_emails);
-        List<String> mailList = new ArrayList<String>();
+        MailGroup mailgroup = new MailGroup(new FileInputStream("resources/grp-1"));
+        List<String> mailList = mailgroup.getMailAddresses();
 
-        MessageStore messageStore = new MessageStore(file_messages);
+        MessageStore messageStore = new MessageStore(new FileInputStream("resources/messages"));
 
         /* Random emails sending */
 
-        output.println(Smtp.CMD_EHLO + domain);
-        output.println(Smtp.CMD_MAIL_FROM + mailgroup.getSender());
-
-        for(String mail : mailList) {
-            output.println(Smtp.CMD_RCPT_TO + mail);
+        LOG.log(Level.INFO,input.readLine());
+        output.println(Smtp.CMD_EHLO + domain + '\r');
+        output.flush();
+        String response;
+        boolean cont = false;
+        while(!cont) {
+            response = input.readLine();
+            LOG.log(Level.INFO, response);
+            if(response.contains("250 ")) {
+                cont = true;
+            }
         }
 
-        output.println(Smtp.CMD_DATA);
-
-        output.println(messageStore.getRandomMessage());
-
-        output.println(Smtp.CMD_END_OF_DATA);
+        output.println(Smtp.CMD_MAIL_FROM + mailgroup.getSender() + '\r');
         output.flush();
+        LOG.log(Level.INFO, input.readLine());
+
+        for(String mail : mailList) {
+            output.println(Smtp.CMD_RCPT_TO + mail + '\r');
+            output.flush();
+            LOG.log(Level.INFO, input.readLine());
+        }
+
+        output.println(Smtp.CMD_DATA + '\r');
+        output.flush();
+        LOG.log(Level.INFO, input.readLine());
+
+        output.println("From : " + mailgroup.getSender() + '\r');
+        output.flush();
+        output.print("To : ");
+        for(String mail : mailList) {
+            output.print(mail + ",");
+        }
+        output.println('\r');
+        output.flush();
+        output.println("Subject : Hello ;)" + '\r');
+        output.println('\r');
+
+        output.println(messageStore.getRandomMessage() + '\r');
+        output.flush();
+
+        output.println("" + '\r');
+        output.flush();
+        output.println(Smtp.CMD_END_OF_DATA + '\r');
+        output.flush();
+        LOG.log(Level.INFO, input.readLine());
+
+        output.println(Smtp.CMD_QUIT + '\r');
+        output.flush();
+
+        clientSocket.close();
+        input.close();
+        output.close();
+
     }
 
 }
--- src/main/java/protocol/Smtp.java
+++ src/main/java/protocol/Smtp.java
@@ -2,9 +2,10 @@ package protocol;
 
 
 public final class Smtp {
-    public static final String CMD_EHLO = "EHLO";
+    public static final String CMD_EHLO = "EHLO ";
     public static final String CMD_MAIL_FROM = "MAIL FROM:";
     public static final String CMD_RCPT_TO = "RCPT TO:";
     public static final String CMD_DATA = "DATA";
     public static final String CMD_END_OF_DATA = ".";
+    public static final String CMD_QUIT = "QUIT";
 }
